f7930e8a1c20132b78493a5cbb8385bd16242a4f
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2024-06-02 04:00:14 +0700

    fix encoding textloader

--- src/chat.py
+++ src/chat.py
@@ -7,46 +7,66 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from argparse import ArgumentParser
+import torch
 
 def get_args():
     parser = ArgumentParser()
     parser.add_argument("--data_dir", type=str, default="data_raw10k")
     parser.add_argument("--persist_dir", type=str, default="chroma_data/")
     parser.add_argument("--col_name", type=str, default="wiki10k")
+    parser.add_argument("--question", type=str, default="Tập đoàn Viettel thành lập năm nào?")
     return parser.parse_args()
 
-args = get_args()
-DATA_DIR = args.data_dir
-CHROMA_PATH = args.persist_dir
-COLLECTION_NAME = args.col_name
+def create_rag_chain(data_dir="data_raw10k", persist_dir="chroma_data/", col_name="wiki10k"):
+    DATA_DIR = data_dir
+    CHROMA_PATH = persist_dir
+    COLLECTION_NAME = col_name
 
-model_name = "keepitreal/vietnamese-sbert"
-embd = HuggingFaceEmbeddings(model_name=model_name)
+    model_name = "keepitreal/vietnamese-sbert"
+    embd = HuggingFaceEmbeddings(model_name=model_name)
 
-vectorstore = Chroma(collection_name=COLLECTION_NAME, persist_directory=CHROMA_PATH, embedding_function=embd)
-retriever = vectorstore.as_retriever()
+    vectorstore = Chroma(collection_name=COLLECTION_NAME, persist_directory=CHROMA_PATH, embedding_function=embd)
+    retriever = vectorstore.as_retriever()
 
-model_id = "vilm/vinallama-2.7b"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
-pipe = pipeline(
-    "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100
-)
-llm = HuggingFacePipeline(pipeline=pipe)
+    model_id = "vilm/vinallama-2.7b"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline(
+        "text-generation", model=model, torch_dtype=torch.float16, tokenizer=tokenizer, max_new_tokens=200, device="cuda"
+    )
+    llm = HuggingFacePipeline(pipeline=pipe)
 
-prompt_template_str = """Bạn là một trợ lý với nhiệm vụ trả lời câu hỏi. Hãy sử dụng những thông tin được cung cấp để trả lời câu hỏi. Nếu bạn không biết hãy trả lời là bạn không biết. Hãy trả lời một cách ngắn gọn và xúc tích.
-Thông tin: {context}
-Câu hỏi: {question}
-Câu trả lời:"""
+    prompt_template_str = """Bạn là một trợ lý với nhiệm vụ trả lời câu hỏi. Hãy sử dụng những thông tin được cung cấp để trả lời câu hỏi. Nếu bạn không biết hãy trả lời là bạn không biết. Hãy trả lời một cách ngắn gọn và xúc tích.
+    Thông tin: {context}
+    Câu hỏi: {question}
+    Câu trả lời:"""
 
-prompt = ChatPromptTemplate.from_template(prompt_template_str)
+    prompt = ChatPromptTemplate.from_message([
+        ("human", prompt_template_str),
+    ])
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs[:1])
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs[:1])
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return rag_chain
+
+def answer(question="Tập đoàn Viettel thành lập năm nào?", chain=None):
+    value = chain.invoke(question)
+    answer = value[value.find("\nCâu trả lời:")+len("\nCâu trả lời:"):]
+    torch.cuda.empty_cache()
+    return answer, question
+    
+if __name__ == "__main__":
+    args = get_args()
+    rag_chain = create_rag_chain(args.data_dir, args.persist_dir, args.col_name)
+    answer, question = answer(args.question, rag_chain)
+    print("Câu hỏi: ", question)
+    print("Trả lời: ", answer)
+
+
--- src/create_data.py
+++ src/create_data.py
@@ -14,26 +14,30 @@ def get_args():
     parser.add_argument("--col_name", type=str, default="wiki10k")
     return parser.parse_args()
 
-args = get_args()
-DATA_DIR = args.data_dir
-CHROMA_PATH = args.persist_dir
-COLLECTION_NAME = args.col_name
+def main():
+    args = get_args()
+    DATA_DIR = args.data_dir
+    CHROMA_PATH = args.persist_dir
+    COLLECTION_NAME = args.col_name
 
-wiki_articles_list = os.listdir(DATA_DIR)
+    wiki_articles_list = os.listdir(DATA_DIR)
 
-model_name = "keepitreal/vietnamese-sbert"
-embd = HuggingFaceEmbeddings(model_name=model_name)
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    model_name = "keepitreal/vietnamese-sbert"
+    embd = HuggingFaceEmbeddings(model_name=model_name)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 
-vectorstore = Chroma(collection_name=COLLECTION_NAME, embedding_function=embd, persist_directory=CHROMA_PATH)
+    vectorstore = Chroma(collection_name=COLLECTION_NAME, embedding_function=embd, persist_directory=CHROMA_PATH)
 
-print("Creating vector store...")
-loop = tqdm(wiki_articles_list)
-for i in loop:
-    loop.set_description(f"Processing {i}")
-    loader = TextLoader(os.path.join(DATA_DIR, i), encoding="utf-8", autodetect_encoding=True)
-    docs = loader.load()
-    splits = splitter.split_documents(docs)
-    vectorstore.add_documents(splits)
+    print("Creating vector store...")
+    loop = tqdm(wiki_articles_list)
+    for i in loop:
+        loop.set_description(f"Processing {i}")
+        loader = TextLoader(os.path.join(DATA_DIR, i), encoding="utf-8", autodetect_encoding=True)
+        docs = loader.load()
+        splits = splitter.split_documents(docs)
+        vectorstore.add_documents(splits)
 
-vectorstore.persist()
+    vectorstore.persist()
+
+if __name__ == "__main__":
+    main()
